760d506a3333911326e24a6c717ab112dd89ed4e
Jonatas Kirsch <jonataskirsch@gmail.com>
2025-12-29 22:40:02 -0300

    Axon config

--- src/main/java/com/jonatask/kyc/infrastructure/config/AxonConfig.java
+++ src/main/java/com/jonatask/kyc/infrastructure/config/AxonConfig.java
@@ -1,8 +1,28 @@
 package com.jonatask.kyc.infrastructure.config;
 
+import com.mongodb.client.MongoClient;
+import org.axonframework.eventhandling.tokenstore.TokenStore;
+import org.axonframework.extensions.mongo.DefaultMongoTemplate;
+import org.axonframework.extensions.mongo.eventsourcing.tokenstore.MongoTokenStore;
+import org.axonframework.serialization.xml.XStreamSerializer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AxonConfig {
-    // Spring Boot auto-configuration will handle Axon based on application.yml
+
+    @Bean
+    public org.axonframework.extensions.mongo.MongoTemplate axonMongoTemplate(MongoClient mongoClient) {
+        return DefaultMongoTemplate.builder()
+            .mongoDatabase(mongoClient, "kyc-db")
+            .build();
+    }
+
+    @Bean
+    public TokenStore tokenStore(org.axonframework.extensions.mongo.MongoTemplate axonMongoTemplate) {
+        return MongoTokenStore.builder()
+            .mongoTemplate(axonMongoTemplate)
+            .serializer(XStreamSerializer.defaultSerializer())
+            .build();
+    }
 }
